14e861917e5cd597ab57b69a24f487f637da2d40
Nanasaheb Pote <32087443+nanapote@users.noreply.github.com>
2017-10-05 08:05:28 +0000

    Update app.js

--- chapter3/app/javascripts/app.js
+++ chapter3/app/javascripts/app.js
@@ -12,7 +12,6 @@ import { default as contract } from 'truffle-contract'
  * to setup a Voting abstraction. We will use this abstraction
  * later to create an instance of the Voting contract.
  * Compare this against the index.js from our previous tutorial to see the difference
- * https://gist.github.com/maheshmurthy/f6e96d6b3fff4cd4fa7f892de8a1a1b4#file-index-js
  */
 
 import voting_artifacts from '../../build/contracts/Voting.json'
